9d89d397d59e69de0c3d926ba2f1b5a28c1838be
Jonathan Murta <jonathan.murta@hotmail.com>
2020-08-02 16:24:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
-const js = (el) => document.querySelector(el)
+const js    = (el) => document.querySelector(el)
 const jsAll = (el) => document.querySelectorAll(el)
-let stateAudio = true
+
+let elementLoad  = null,
+    stateAudio   = false
 
 const interval = setInterval(() =>
 {
-    const elementLoad = js("._3NrAe")
+    elementLoad = js("._3NrAe")
     
     if(elementLoad)
     {
         clearInterval(interval);
 
         const iconSpeed = document.createElement("span")
-        iconSpeed.innerHTML = "2x"
+        iconSpeed.innerHTML = "1x"
         iconSpeed.classList.add("speedAudio")
         elementLoad.prepend(iconSpeed)
 
         iconSpeed.addEventListener("click", () =>
         {
-            const audios = document.querySelectorAll("audio")
-
-            audios.forEach((element) =>
-            {
-                if (stateAudio)
-                {
-                    stateAudio = false
-                    element.playbackRate = 1.5;
-                    iconSpeed.innerHTML = "1x"
-                }
-                else
-                {
-                    stateAudio = true
-                    element.playbackRate = 1;
-                    iconSpeed.innerHTML = "2x"
-                }
-            })
+            stateAudio = !stateAudio
+            stateAudio ? js("span.speedAudio").innerHTML = "2x" : js("span.speedAudio").innerHTML = "1x"
+            ActionAudio()
         })
     }
 }, 1000)
+
+const intervalAmount = setInterval(() =>
+{
+    if(document.querySelector(".z_tTQ audio") && !document.querySelector(".z_tTQ audio").classList.contains("backRate"))
+        ActionAudio()
+}, 1000)
+
+ActionAudio = () =>
+{
+    const audios = document.querySelectorAll("audio")
+
+    audios.forEach(element =>
+    {        
+        if(stateAudio)
+        {
+            element.playbackRate = 2;
+            element.classList.add("backRate")
+        }
+        else
+        {
+            element.playbackRate = 1;
+            element.classList.remove("backRate")
+        }   
+    })
+}
    
